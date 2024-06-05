2edf22eaaec13fb9f816a284fbbd2880f15f0dad
moshebeutel <moshebeutel@gmail.com>
2024-06-05 21:57:45 +0300

    repo reorganization from old repo

--- keypressemg/models/not_used/factory.py
+++
@@ -1,9 +0,0 @@
-from models.feature_model import FeatureModel
-from models.not_used.window_model import WindowModel
-
-
-def get_model(args):
-    dataset_name = args.dataset_name
-    assert dataset_name in ['signal_windows', 'signal_features'], (f'Dataset {dataset_name} not supported.'
-                                                                   f' Expected windows or features.')
-    return WindowModel(use_group_norm=False, use_dropout=False) if dataset_name == 'signal_windows' else FeatureModel()
